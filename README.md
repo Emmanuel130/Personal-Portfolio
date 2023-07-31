bd084b875bd3ffee9dbeb8525bbd5e9c356310be
Amanuel Detebo <85768416+Emmanuel130@users.noreply.github.com>
2023-07-31 15:46:33 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,66 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Personal Portfolio 
+
+> In this project, I set up a "Personal Portfolio" repository and I make the mobile version of part of the website. I also added the proper linters for html and css files.
+
+
+## Built With
+
+- HTML
+- css
+
+## Getting Started
+
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+There are no prerequisites for this project.
+
+### Setup
+
+Clone the repository and open the index.html file.
+
+### Install
+
+Install all the dependencies by running the command npm -i
+
+## Live Demo 
+
+[Demo](https://abe1able.github.io/Personal-Portfolio/)
+
+## Authors
+
+
+👤 **Author**
+
+
+- GitHub: [@abe1able](https://github.com/abe1able)
+- Twitter: [@AbelSeifu15](https://twitter.com/AbelSeifu15)
+- Linkedin: [@abelseifu](https://www.linkedin.com/in/abel-seifu-184543233/)
+
+
+## Contributor
+
+- GitHub: [@Irirwanirira](https://github.com/Irirwanirira)
+- Twitter: [@Iri_joseph](https://twitter.com/Irirwanirira)
+- LinkedIn: [Joseph IRIRWANIRIRA](https://linkedin.com/in/joseph-irirwanirira-74666623a/)
+
+
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+Feel free to check the [issues page](https://github.com/Abe1able/Personal-Portfolio/issues)
+
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+
+## 📝 License
+
+This project is [MIT](./LICENSE) licensed.
---
+++ script.js
@@ -0,0 +1,196 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu")
+
+hamburger.addEventListener ("click", () =>{
+
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+})
+
+document.querySelectorAll(".nav-link").forEach( n => n. 
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+
+    })
+)
+
+
+function showMessage(input, message, type) {
+    const msg = input.parentNode.querySelector('small');
+    msg.innerText = message;
+    return type;
+  }
+  
+  function showError(input, message) {
+    return showMessage(input, message, false);
+  }
+  
+  function showSuccess(input) {
+    return showMessage(input, '', true);
+  }
+  
+  function hasValue(input, message) {
+    if (input.value.trim() === '') {
+      return showError(input, message);
+    }
+    return showSuccess(input);
+  }
+  
+  function validateEmail(input, invalidMsg) {
+    if (!hasValue(input, invalidMsg)) {
+      return false;
+    }
+    const emailRegex = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+  
+    const email = input.value.trim();
+    if (!emailRegex.test(email)) {
+      return showError(input, invalidMsg);
+    }
+    return true;
+  }
+  
+  const form = document.querySelector('#myForm');
+  const EMAIL_INVALID = 'Please enter a correct email address format. Uppercase not allowed in this field!!';
+  
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const emailValid = validateEmail(form.elements.email, EMAIL_INVALID);
+    if (emailValid) {
+      form.submit();
+    }
+  });
+
+const mainTag = document.querySelector('.main');
+const header = document.querySelector('.header');
+const seeProjectDetail = document.querySelectorAll('.see-project-btn');
+const projectDetail = document.querySelector('.project-detail');
+
+const projects = [
+  {
+    id: '1',
+    name: 'Tonic',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+    featured_image: './Images/camara.png',
+    link_to_live: ['See live', './Images/detailProjectLive.png', 'https://emmanuel130.github.io/Cmara-Education-Volunteer-website-final/#home'],
+    link_to_source: ['See source', './Images/detailProjectGit.png', 'https://github.com/Emmanuel130/Cmara-Education-Volunteer-website-final'],
+    technologies: ['html', 'css', 'javascript', 'react'],
+    name_detail: ['Canopy', 'Back-End Dev', '2015'],
+  },
+  {
+    id: '2',
+    name: 'Multi-post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featured_image: './Images/adama.png',
+    link_to_live: ['See live', './Images/detailProjectLive.png', 'https://emmanuel130.github.io/Admacity-project/'],
+    link_to_source: ['See source', './Images/detailProjectGit.png', 'https://github.com/Emmanuel130/Admacity-project'],
+    technologies: ['html', 'css', 'javascript'],
+    name_detail: ['Abel Seifu', 'Front-End Dev', '2022'],
+  },
+  {
+    id: '3',
+    name: 'Tonic 3',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featured_image: './Images/Snapshoot.png',
+    link_to_live: ['See live', './Images/detailProjectLive.png', '#'],
+    link_to_source: ['See source', './Images/detailProjectGit.png', '#'],
+    technologies: ['html', 'css', 'javascript'],
+    name_detail: ['Abel Seifu', 'Front-End Dev', '2022'],
+  },
+  {
+    id: '4',
+    name: 'Tonic 4',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featured_image: './Images/Snapshoot.png',
+    link_to_live: ['See live', './Images/detailProjectLive.png', '#'],
+    link_to_source: ['See source', './Images/detailProjectGit.png', '#'],
+    technologies: ['html', 'css', 'javaScript', 'Git', 'Webpack'],
+    name_detail: ['Abel Seifu', 'Front-End Dev', '2022'],
+  },
+];
+
+seeProjectDetail.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const selectedProject = projects.filter((project) => project.id === e.target.id)[0];
+    projectDetail.classList.add('active');
+    mainTag.classList.add('fixed_postion');
+    header.classList.add('fixed_postion');
+    projectDetail.innerHTML = `
+        <div class="project-detail-background"></div>
+        <article class="work-container flex project-detail-article project-flex">
+            <div class="project-detail-header">
+              <h3>${selectedProject.name}</h3>
+              <button data-close-button class="close-button-project">&times;</button>
+           </div>
+           <div class="client-des">
+              <ul class="client-name">
+                <li>${selectedProject.name_detail[0]}</li>
+              </ul>
+              <ul>
+                <li>${selectedProject.name_detail[1]}</li>
+              </ul>
+              <ul>
+                <li>${selectedProject.name_detail[2]}</li>
+              </ul>
+            </div>
+          <div class="work-img" style="background: url(${selectedProject.featured_image});
+          background-size: cover; width: auto"></div>
+          <div class="work-description-project project-detail-work-description">
+            <p>
+              ${selectedProject.description}
+            </p>
+            <div class="project-detail-tech-btns">
+            <ul class="buttons project-detail-buttons">
+              <li><button type="button">${selectedProject.technologies[0]}</button></li>
+              <li><button type="button">${selectedProject.technologies[1]}</button></li>
+              <li><button type="button">${selectedProject.technologies[2]}</button></li>
+              <li><button type="button">${selectedProject.technologies[3]}</button></li>
+            </ul>
+            <div class="project-detail-buttons">
+                <a href="${selectedProject.link_to_live[2]}"><button class="see-project-btn see-source">${selectedProject.link_to_live[0]}
+                  <img class="see-source-img" src=${selectedProject.link_to_live[1]} alt="" /></button>
+                </a>
+                <a href="${selectedProject.link_to_source[2]}"><button class="see-project-btn see-source">${selectedProject.link_to_source[0]}
+                  <img class="see-source-img" src=${selectedProject.link_to_source[1]} alt="" /></button>
+                </a>
+            </div>
+            </div>
+          </div>
+        </article>
+        `;
+    const projectDetailClose = document.querySelector('.close-button-project');
+    projectDetailClose.addEventListener('click', () => {
+      mainTag.classList.remove('fixed_postion');
+      header.classList.remove('fixed_postion');
+      projectDetail.classList.remove('active');
+    });
+  });
+});
+
+
+const fullNameForm = document.getElementById('fullName');
+const emailForm = document.getElementById('email');
+const commentForm = document.getElementById('comment');
+
+function handleChange() {
+  const formData = {
+    fullName: fullNameForm.value,
+    email: emailForm.value,
+    comment: commentForm.value,
+  };
+  localStorage.setItem('form', JSON.stringify(formData));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const getFormValue = localStorage.getItem('form');
+  if (getFormValue) {
+    const formObject = JSON.parse(getFormValue);
+    fullNameForm.value = formObject.fullName;
+    emailForm.value = formObject.email;
+    commentForm.value = formObject.comment;
+  }
+});
+
+fullNameForm.onchange = handleChange;
+emailForm.onchange = handleChange;
+commentForm.onchange = handleChange;
